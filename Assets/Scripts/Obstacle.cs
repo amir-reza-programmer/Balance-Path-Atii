71a81b52d00c955062c9a7b1c46e36791be3c6cf
amir-reza-programmer <76550627+amir-reza-programmer@users.noreply.github.com>
2026-08-15 19:17:55 +0330

    Update Obstacle.cs

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -2,13 +2,12 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
-    public float radius = 0.25f; // Size of the penalty zone
+    public float radius = 0.25f;
     public bool isHit = false;
 
-    // Checks if the foot position overlaps the obstacle 
+    // if the foot position overlaps 
     public bool CheckFootPosition(Vector3 footPosition)
     {
-        // Convert 3D world position to flat 2D math (ignoring height)
         Vector2 obstaclePos = new Vector2(transform.position.x, transform.position.z);
         Vector2 footPos2D = new Vector2(footPosition.x, footPosition.z);
         
